837e5d8f6490098c7f31a6c9838b65ad9cdef487
SuenjeAlice <gmit.winteler@gmail.com>
2022-03-19 18:00:18 +0100

    Event System

--- Gemgine/src/Gemgine/Application.cpp
+++ Gemgine/src/Gemgine/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Gemgine/Events/ApplicationEvent.h"
+#include "Gemgine/Log.h"
+
 namespace Gemgine {
 	
 	Application::Application()
@@ -14,6 +17,9 @@ namespace Gemgine {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		GG_TRACE(e);
+
 		while (true);
 	}
 }
--- premake5.lua
+++ premake5.lua
@@ -26,6 +26,7 @@ project "Gemgine"
 
 	includedirs
 	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
